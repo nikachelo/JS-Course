eb542dd76ac0b10be0523303c18444e01f857a05
nikachelo <nikachelidze9@gmail.com>
2024-02-19 15:42:36 +0400

    First AJAX call with old method

---
+++ Async JS - Lectures/Async - notes.md
@@ -0,0 +1,22 @@
+246 - Async JS, AJAX, APIs
+
+Synchronous code -
+
+1.  Most code is synchronous
+2.  Synchronous code is executed line by line
+3.  Each Line of code waits for previous line to finish
+4.  Long running operations block code execution
+
+Asynchronous code -
+
+1. It is executed after a task that runds in the "background" finishes;
+2. Async code is non-blocking
+3. Executed doesn't wait for async task to finish its work
+
+AJAX Calls - Async JS And XML allows us to communicate with remote web servers in an async way. With AJAX calls, we can request data from web servers dynamically.
+
+API - Application Programming Interface - Piece of software that can be used by another piece of software, in order to allow applications to talk to each other.
+
+249 - How the web works
+
+TCP - Transmission Control Protocol
--- Async JS - Lectures/script.js
+++ Async JS - Lectures/script.js
@@ -4,3 +4,33 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const getCountries = function (country) {
+  const request = new XMLHttpRequest(); //Old way of doing that
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const response = JSON.parse(this.responseText);
+    const [data] = response;
+    console.log(data);
+    const html = `
+  <article class="country">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${data.population} people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+</article>
+  `;
+    countriesContainer.insertAdjacentHTML('afterbegin', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountries('georgia');
+getCountries('ghana');
+getCountries('spain');
+getCountries('america');
